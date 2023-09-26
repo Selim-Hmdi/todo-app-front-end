dc46ca93d27a551f14f236a61bb4904bb6d1a7c4
Selim Hoummadi <selim_hoummadi@hotmail.com>
2023-09-26 15:09:01 +0200

    Remove auto generated conf and files and add layout
    Auto generated file are deleted or changed
    Add AppLayout HeaderLayout and HomePage which compose app's
    basic structure

---
+++ src/layout/TodoAppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1 class="app-title">Todo app</h1>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.app-title {
+    text-align: center;
+}
+</style>
---
+++ src/layout/TodoAppLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <TodoAppHeader></TodoAppHeader>
+    <slot></slot>
+</template>
+<script>
+import TodoAppHeader from './TodoAppHeader.vue';
+
+export default {
+    components: { TodoAppHeader }
+}
+</script>
